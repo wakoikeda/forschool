cbc094317288976d6d6b30fc3bff6ba1a7f88d72
wakoikeda <wakowako1009@icloud.com>
2024-11-01 10:57:50 +0900

    ファビコン作成

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
+   <link rel="icon" href="{{ asset('build/assets/favicon.png') }}" type="image/png">
+
+
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
